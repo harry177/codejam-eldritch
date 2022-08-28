fbf5b3b1bf157bc6c2004feab8d6c2d1ce6e978b
Harry177 <harry177@rambler.ru>
2022-08-28 23:00:02 +0200

    fix: change images path

--- assets/MythicCards/brown/browncards.js
+++ assets/MythicCards/brown/browncards.js
@@ -1,127 +1,127 @@
 const brownCards = [
     {
         title: 'brown1',
-        src: 'url(../assets/MythicCards/brown/brown1.png)',
+        src: 'url(./assets/MythicCards/brown/brown1.png)',
         level: 'normal',
         color: 'brown'
     },
     {
         title: 'brown2',
-        src: 'url(../assets/MythicCards/brown/brown2.png)',
+        src: 'url(./assets/MythicCards/brown/brown2.png)',
         level: 'normal',
         color: 'brown'
     },
     {
         title: 'brown3',
-        src: 'url(../assets/MythicCards/brown/brown3.png)',
+        src: 'url(./assets/MythicCards/brown/brown3.png)',
         level: 'normal',
         color: 'brown'
     },
     {
         title: 'brown4',
-        src: 'url(../assets/MythicCards/brown/brown4.png)',
+        src: 'url(./assets/MythicCards/brown/brown4.png)',
         level: 'normal',
         color: 'brown'
     },
     {
         title: 'brown5',
-        src: 'url(../assets/MythicCards/brown/brown5.png)',
+        src: 'url(./assets/MythicCards/brown/brown5.png)',
         level: 'normal',
         color: 'brown'
     },
     {
         title: 'brown6',
-        src: 'url(../assets/MythicCards/brown/brown6.png)',
+        src: 'url(./assets/MythicCards/brown/brown6.png)',
         level: 'hard',
         color: 'brown'
     },
     {
         title: 'brown7',
-        src: 'url(../assets/MythicCards/brown/brown7.png)',
+        src: 'url(./assets/MythicCards/brown/brown7.png)',
         level: 'hard',
         color: 'brown'
     },
     {
         title: 'brown8',
-        src: 'url(../assets/MythicCards/brown/brown8.png)',
+        src: 'url(./assets/MythicCards/brown/brown8.png)',
         level: 'hard',
         color: 'brown'
     },
     {
         title: 'brown9',
-        src: 'url(../assets/MythicCards/brown/brown9.png)',
+        src: 'url(./assets/MythicCards/brown/brown9.png)',
         level: 'hard',
         color: 'brown'
     },
     {
         title: 'brown10',
-        src: 'url(../assets/MythicCards/brown/brown10.png)',
+        src: 'url(./assets/MythicCards/brown/brown10.png)',
         level: 'hard',
         color: 'brown'
     },
     {
         title: 'brown11',
-        src: 'url(../assets/MythicCards/brown/brown11.png)',
+        src: 'url(./assets/MythicCards/brown/brown11.png)',
         level: 'easy',
         color: 'brown'
     },
     {
         title: 'brown12',
-        src: 'url(../assets/MythicCards/brown/brown12.png)',
+        src: 'url(./assets/MythicCards/brown/brown12.png)',
         level: 'easy',
         color: 'brown'
     },
     {
         title: 'brown13',
-        src: 'url(../assets/MythicCards/brown/brown13.png)',
+        src: 'url(./assets/MythicCards/brown/brown13.png)',
         level: 'easy',
         color: 'brown'
     },
     {
         title: 'brown14',
-        src: 'url(../assets/MythicCards/brown/brown14.png)',
+        src: 'url(./assets/MythicCards/brown/brown14.png)',
         level: 'easy',
         color: 'brown'
     },
     {
         title: 'brown15',
-        src: 'url(../assets/MythicCards/brown/brown15.png)',
+        src: 'url(./assets/MythicCards/brown/brown15.png)',
         level: 'normal',
         color: 'brown'
     },
     {
         title: 'brown16',
-        src: 'url(../assets/MythicCards/brown/brown16.png)',
+        src: 'url(./assets/MythicCards/brown/brown16.png)',
         level: 'normal',
         color: 'brown'
     },
     {
         title: 'brown17',
-        src: 'url(../assets/MythicCards/brown/brown17.png)',
+        src: 'url(./assets/MythicCards/brown/brown17.png)',
         level: 'normal',
         color: 'brown'
     },
     {
         title: 'brown18',
-        src: 'url(../assets/MythicCards/brown/brown18.png)',
+        src: 'url(./assets/MythicCards/brown/brown18.png)',
         level: 'normal',
         color: 'brown'
     },
     {
         title: 'brown19',
-        src: 'url(../assets/MythicCards/brown/brown19.png)',
+        src: 'url(./assets/MythicCards/brown/brown19.png)',
         level: 'normal',
         color: 'brown'
     },
     {
         title: 'brown20',
-        src: 'url(../assets/MythicCards/brown/brown20.png)',
+        src: 'url(./assets/MythicCards/brown/brown20.png)',
         level: 'normal',
         color: 'brown'
     },
     {
         title: 'brown21',
-        src: 'url(../assets/MythicCards/brown/brown21.png)',
+        src: 'url(./assets/MythicCards/brown/brown21.png)',
         level: 'easy',
         color: 'brown'
     }
--- assets/MythicCards/green/greencards.js
+++ assets/MythicCards/green/greencards.js
@@ -1,109 +1,109 @@
 let greenCards = [
     {
         title: 'green1',
-        src: 'url(../assets/MythicCards/green/green1.png)',
+        src: 'url(./assets/MythicCards/green/green1.png)',
         level: 'easy',
         color: 'green'
     },
     {
         title: 'green2',
-        src: 'url(../assets/MythicCards/green/green2.png)',
+        src: 'url(./assets/MythicCards/green/green2.png)',
         level: 'hard',
         color: 'green'
     },
     {
         title: 'green3',
-        src: 'url(../assets/MythicCards/green/green3.png)',
+        src: 'url(./assets/MythicCards/green/green3.png)',
         level: 'hard',
         color: 'green'
     },
     {
         title: 'green4',
-        src: 'url(../assets/MythicCards/green/green4.png)',
+        src: 'url(./assets/MythicCards/green/green4.png)',
         level: 'hard',
         color: 'green'
     },
     {
         title: 'green5',
-        src: 'url(../assets/MythicCards/green/green5.png)',
+        src: 'url(./assets/MythicCards/green/green5.png)',
         level: 'hard',
         color: 'green'
     },
     {
         title: 'green6',
-        src: 'url(../assets/MythicCards/green/green6.png)',
+        src: 'url(./assets/MythicCards/green/green6.png)',
         level: 'hard',
         color: 'green'
     },
     {
         title: 'green7',
-        src: 'url(../assets/MythicCards/green/green7.png)',
+        src: 'url(./assets/MythicCards/green/green7.png)',
         level: 'normal',
         color: 'green'
     },
     {
         title: 'green8',
-        src: 'url(../assets/MythicCards/green/green8.png)',
+        src: 'url(./assets/MythicCards/green/green8.png)',
         level: 'normal',
         color: 'green'
     },
     {
         title: 'green9',
-        src: 'url(../assets/MythicCards/green/green9.png)',
+        src: 'url(./assets/MythicCards/green/green9.png)',
         level: 'normal',
         color: 'green'
     },
     {
         title: 'green10',
-        src: 'url(../assets/MythicCards/green/green10.png)',
+        src: 'url(./assets/MythicCards/green/green10.png)',
         level: 'normal',
         color: 'green'
     },
     {
         title: 'green11',
-        src: 'url(../assets/MythicCards/green/green11.png)',
+        src: 'url(./assets/MythicCards/green/green11.png)',
         level: 'normal',
         color: 'green'
     },
     {
         title: 'green12',
-        src: 'url(../assets/MythicCards/green/green12.png)',
+        src: 'url(./assets/MythicCards/green/green12.png)',
         level: 'easy',
         color: 'green'
     },
     {
         title: 'green13',
-        src: 'url(../assets/MythicCards/green/green13.png)',
+        src: 'url(./assets/MythicCards/green/green13.png)',
         level: 'normal',
         color: 'green'
     },
     {
         title: 'green14',
-        src: 'url(../assets/MythicCards/green/green14.png)',
+        src: 'url(./assets/MythicCards/green/green14.png)',
         level: 'normal',
         color: 'green'
     },
     {
         title: 'green15',
-        src: 'url(../assets/MythicCards/green/green15.png)',
+        src: 'url(./assets/MythicCards/green/green15.png)',
         level: 'normal',
         color: 'green'
     },
     {
         title: 'green16',
-        src: 'url(../assets/MythicCards/green/green16.png)',
+        src: 'url(./assets/MythicCards/green/green16.png)',
         level: 'easy',
         color: 'green'
     },
     {
         title: 'green17',
-        src: 'url(../assets/MythicCards/green/green17.png)',
+        src: 'url(./assets/MythicCards/green/green17.png)',
         level: 'easy',
         color: 'green'
     },
     {
         title: 'green18',
-        src: 'url(../assets/MythicCards/green/green18.png)',
+        src: 'url(./assets/MythicCards/green/green18.png)',
         level: 'easy',
         color: 'green'
     }
